ae55b62e31235f4d3142763f10e38ed3e1bee499
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2019-08-29 10:32:25 +0300

    refactor to maven project

--- src/com/acme/Main.java
+++
@@ -1,8 +0,0 @@
-package com.acme;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
--- src/com/acme/basics/RunnableDemo.java
+++
@@ -1,7 +0,0 @@
-package com.acme.basics;
-
-public class RunnableDemo {
-    public static void main(String[] args) {
-        new Thread(() -> System.out.println(Thread.currentThread().getName())).start();
-    }
-}
