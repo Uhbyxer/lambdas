63039e8413829b73b8c9f5a7af770f903a74f5f6
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2019-09-03 08:59:20 +0300

    optional

---
+++ src/main/java/com/acme/optional/OptionalDemo.java
@@ -0,0 +1,36 @@
+package com.acme.optional;
+
+import com.acme.common.Customer;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import static java.util.Arrays.asList;
+
+public class OptionalDemo {
+	public static void main(String[] args) {
+
+		//Returns NLP exception
+		try {
+			Customer customer = null;
+			Optional<Customer> cus = Optional.of(customer);
+			System.out.println(cus);
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		}
+
+		Supplier<Customer> customerSupplier = () -> new Customer("Zed", "Kiutza", 35, "Dr", asList("EUR", "UAH"));
+		Optional<Customer> customerOptional = Optional.ofNullable(customerSupplier.get());
+
+		customerOptional.ifPresent(System.out::println);
+
+		//isPresent
+		Optional<String> name = customerOptional.map(Customer::getName);
+		name.ifPresent(s -> System.out.println("Name: " + s));
+
+		//Optional.empty
+		Optional<Object> empty = Optional.empty();
+		System.out.println(empty);
+
+	}
+}
